e09c8d01d6f53c16ce176dc45d752b70a2f71358
Ar1ew0rk <artework.gamedev@gmail.com>
2026-08-12 18:32:36 +0200

    2.9 Camera limits

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -1,13 +1,28 @@
 extends Camera2D
 
 var player: Player = null
+var viewport_size: Vector2
+
+const CAMERA_MARGIN: int = 460
 
 func _ready() -> void:
-	global_position.x = get_viewport_rect().size.x / 2
+	viewport_size = get_viewport_rect().size
+	
+	# Set the camera position
+	limit_bottom = int(viewport_size.y)
+	limit_left = 0
+	limit_right = int(viewport_size.x)
+
+func _process(_delta: float) -> void:
+	# Stop the camera from moving down
+	if player:
+		if limit_bottom > player.global_position.y + CAMERA_MARGIN:
+			limit_bottom = int(player.global_position.y + CAMERA_MARGIN)
 
 func _physics_process(_delta: float) -> void:
 	if player == null:
 		return
+	# Track the player's position
 	global_position.y = player.global_position.y
 
 func setup_camera(_player: Player) -> void:
