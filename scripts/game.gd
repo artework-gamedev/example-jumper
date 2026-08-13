629617308fb38265e3dc1d39c08af523c3f54b1e
Ar1ewOrk <artework.gamedev@gmail.com>
2026-08-13 16:43:59 +0200

    2.10 Level generation base + debug inputs

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,29 @@
 extends Node2D
 
-var camera_scene = preload("res://scenes/game_camera.tscn")
-var camera : Camera2D = null
+# Camera globals
+var camera_scene: Resource = preload("res://scenes/game_camera.tscn")
+var camera: Camera2D = null
+
+# Platform generation globals
+@onready var platform_parent = $PlatformParent
+var platform_scene: Resource = preload("res://scenes/Platform.tscn")
 
 func _ready() -> void:
 	camera = camera_scene.instantiate()
 	camera.setup_camera($Player)
 	add_child(camera)
+	
+	create_platform(Vector2(100, 300))
+
+func _process(_delta: float) -> void:
+	# Process debug commands to quit and reset game
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+
+func create_platform(location: Vector2) -> Node2D:
+	var platform: Node2D = platform_scene.instantiate()
+	platform.global_position = location
+	platform_parent.add_child(platform)
+	return platform
