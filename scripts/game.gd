cb66dc75c2cee23e618b733f9fbf7da2752fb89e
Ar1ewOrk <artework.gamedev@gmail.com>
2026-08-13 17:02:54 +0200

    2.11 Creating the ground

--- scripts/game.gd
+++ scripts/game.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	camera.setup_camera($Player)
 	add_child(camera)
 	
-	create_platform(Vector2(100, 300))
+	generate_ground_layer_of_platforms()
 
 func _process(_delta: float) -> void:
 	# Process debug commands to quit and reset game
@@ -27,3 +27,13 @@ func create_platform(location: Vector2) -> Node2D:
 	platform.global_position = location
 	platform_parent.add_child(platform)
 	return platform
+
+func generate_ground_layer_of_platforms() -> void:
+	const platform_width: int = 136 # Hardcoded value, taken from the width of the CollisionShape2D rectangle of the platform
+	const platform_height: int = 63 # Hardcoded, taken from the height of the platform sprite
+	var viewport_size: Vector2 = get_viewport_rect().size
+	# Number of platforms needed to cover the ground
+	var num_ground_layer_platforms: float = (viewport_size.x / platform_width) + 1
+	# Cover the ground in the platforms
+	for i in range(num_ground_layer_platforms):
+		create_platform(Vector2(i * platform_width, viewport_size.y - platform_height))
