3bae62765760812397b171318cfe26ffa6d7fd2e
Ar1ewOrk <artework.gamedev@gmail.com>
2026-08-14 02:16:59 +0200

    2.20 Parallax backgrounds

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,6 +3,7 @@ extends Node2D
 # Camera globals
 var camera_scene: Resource = preload("res://scenes/game_camera.tscn")
 var camera: Camera2D = null
+var viewport_size: Vector2
 
 # Player instantiation
 var player_scene: Resource = preload("res://scenes/Player.tscn")
@@ -13,19 +14,28 @@ var player_spawn_pos: Vector2 = Vector2.ZERO
 # Level generator instantiation
 @onready var level_generator: Node2D = $LevelGenerator
 
-# Background sprites
+# Background sprites and parallaxes
 @onready var ground_sprite: Sprite2D = $GroundSprite
+@onready var parallax1: Parallax2D = $ParallaxBackround/Parallax2D
+@onready var parallax2: Parallax2D = $ParallaxBackround/Parallax2D2
+@onready var parallax3: Parallax2D = $ParallaxBackround/Parallax2D3
 
 
 func _ready() -> void:
-	var viewport_size = get_viewport_rect().size
+	viewport_size = get_viewport_rect().size
 	player_spawn_pos.x = viewport_size.x / 2.0
 	player_spawn_pos.y = viewport_size.y - player_spawn_offset
 	
+	# Set up the ground sprite dimensions
 	ground_sprite.global_position.x = viewport_size.x / 2
 	ground_sprite.global_position.y = viewport_size.y
-	const ground_sprite_width: int = 800 # Hardcoded texture size
+	var ground_sprite_width: float = ground_sprite.texture.get_size().x
 	ground_sprite.scale.x = viewport_size.x / ground_sprite_width
+	
+	# Set up the parallax background layers
+	configure_parallax(parallax1, 0.1)
+	configure_parallax(parallax2, 0.5)
+	configure_parallax(parallax3, 0.85)
 	
 	new_game()
 
@@ -48,3 +58,15 @@ func new_game() -> void:
 	add_child(camera)
 	
 	level_generator.setup(player)
+
+func configure_parallax(parallax: Parallax2D, scroll_scale: float) -> void:
+	var sprite: Sprite2D = parallax.find_child("Sprite2D")
+	if sprite == null:
+		print("Could not configure " + parallax.name)
+		return
+	var texture_size: Vector2 = sprite.texture.get_size()
+	var texture_scale_factor: float = viewport_size.x / texture_size.x
+	sprite.position.x = viewport_size.x / 2
+	sprite.scale = Vector2.ONE * texture_scale_factor
+	parallax.repeat_size = Vector2(0, texture_size.y * texture_scale_factor)
+	parallax.scroll_scale.y = scroll_scale
